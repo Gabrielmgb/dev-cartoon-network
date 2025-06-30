26f5e5ae829006182d25d31ec5d0fc6f7e524144
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-06-30 19:50:54 -0300

    CNlogo

--- components/cn-logo.tsx
+++ components/cn-logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 const CNLogo = () => (
   <Link href="/" aria-label="Cartoon Network Home">
     <Image
-      src="/logo-CN.png" // Caminho da imagem dentro da pasta /public
+      src="/dev-cartoon-network/logo-CN.png" // Caminho da imagem dentro da pasta /public
       alt="Cartoon Network Logo"
       width={120}
       height={60}
