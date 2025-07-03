05da6ec0895058f70cdd39ae69b05c7b5ae31a75
Gabriel Magalhães <gabrielmgb.nd@gmail.com>
2025-07-03 05:13:09 -0300

    Updates

---
+++ _next/static/chunks/app/liga-toon/page-db98bf7fa915feff.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[487,639],{5574:(e,s,n)=>{Promise.resolve().then(n.bind(n,4155)),Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[467,155,441,684,358],()=>s(5574)),_N_E=e.O()}]);
